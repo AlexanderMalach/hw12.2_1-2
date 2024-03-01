c6282128675cbea48c730ee69838dfc80ba02424
79096 <a.malakhinsky@gmail.com>
2024-03-01 23:09:03 +0300

    Cover 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,20 @@
 from utils import arrs
 
-
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
+    assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
-
+    assert arrs.get([1, 2, 3], -1, "test") == "test"
+    assert arrs.get([], -1, "test") == "test"
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], end=3) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], -3, 3) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], 1, -1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], -3, -1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], -10, 3) == [1, 2, 3]
+    assert arrs.my_slice([1, 2, 3, 4], 1, -10) == []
+    assert arrs.my_slice([1, 2, 3, 4], -10, -5) == []
+    assert arrs.my_slice([1, 2, 3, 4], -10, -1) == [1, 2, 3]
+    assert arrs.my_slice([]) == []
